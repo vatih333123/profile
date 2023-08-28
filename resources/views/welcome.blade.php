642847b4a8b74b2bead1651debf87a87e0d24e28
vatih333123 <142955107+vatih333123@users.noreply.github.com>
2023-08-28 13:48:09 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,140 @@
+                       <html>
+                            <head>
+                                <meta charset='utf-8'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <title>Profile</title>
+                                <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' rel='stylesheet'>
+                                <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
+                                <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+                                <style>::-webkit-scrollbar {
+                                  width: 8px;
+                                }
+                                /* Track */
+                                ::-webkit-scrollbar-track {
+                                  background: #f1f1f1; 
+                                }
+                                 
+                                /* Handle */
+                                ::-webkit-scrollbar-thumb {
+                                  background: #888; 
+                                }
+                                
+                                /* Handle on hover */
+                                ::-webkit-scrollbar-thumb:hover {
+                                  background: #555; 
+                                } @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Noto Sans', sans-serif;
+    
+}
+
+html,body{
+    
+    height: 100%;
+    
+}
+
+body{
+    
+    display: grid;
+    place-items:center;
+    background: #eee;
+}
+
+
+.card{
+    
+    width:350px;
+    border-radius: 50px;
+background: #eeeeee;
+box-shadow:  20px 20px 60px #cacaca, 
+             -20px -20px 60px #ffffff;
+    border:none;
+    
+}
+
+.neo-button {
+    width: 40px;
+    height: 40px;
+    outline: 0 !important;
+    cursor: pointer;
+    color: #fff;
+    font-size: 15px;
+    border: none;
+    margin-right: 10px;
+    border-radius: 50%;
+background: linear-gradient(145deg, #d6d6d6, #ffffff);
+box-shadow:  20px 20px 60px #cacaca, 
+             -20px -20px 60px #ffffff;
+}
+
+.neo-button:active {
+    border-radius: 50%;
+background: linear-gradient(145deg, #d6d6d6, #ffffff);
+box-shadow: inset 20px 20px 60px #cacaca, 
+            inset -20px -20px 60px #ffffff;
+}
+
+.fa-facebook {
+    color: #3b5998
+}
+
+.fa-linkedin {
+    color: #0077b5
+}
+ 
+.fa-google { 
+    color: #dc4e41
+}
+
+.fa-youtube {
+    color: #cd201f
+}
+
+.fa-twitter {
+    color: #55acee
+}</style>
+                                </head>
+                                <body className='snippet-body'>
+                                <div class="container d-flex justify-content-center">
+                 
+                 <div class="card p-3 py-4">
+                     
+                     <div class="text-center">
+                         
+                         <img src="https://images.unsplash.com/photo-1588768987479-bcebeefb8a5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" width="100" class="rounded-circle">
+
+                         <h3 class="mt-2">Vatih Miftakhul ulum</h3>
+                         <span class="mt-1 clearfix">Student</span>
+                         <small class="mt-4">I am an student from SMK Bhina Karya</small>
+                         
+                         <div class="social-buttons mt-5"> 
+                             <button class="neo-button"><i class="fa fa-facebook fa-1x"></i> </button> 
+                             <button class="neo-button"><i class="fa fa-linkedin fa-1x"></i></button> 
+                             <button class="neo-button"><i class="fa fa-google fa-1x"></i> </button> 
+                             <button class="neo-button"><i class="fa fa-youtube fa-1x"></i> </button> 
+                             <button class="neo-button"><i class="fa fa-twitter fa-1x"></i> </button> 
+                         </div>
+                     
+                     </div>
+                     
+                 
+                 </div>
+    
+             </div>
+                                <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js'></script>
+                                <script type='text/javascript' src='#'></script>
+                                <script type='text/javascript' src='#'></script>
+                                <script type='text/javascript' src='#'></script>
+                                <script type='text/javascript'>#</script>
+                                <script type='text/javascript'>var myLink = document.querySelector('a[href="#"]');
+                                myLink.addEventListener('click', function(e) {
+                                  e.preventDefault();
+                                });</script>
+                            
+                                </body>
+                            </html>
